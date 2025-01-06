30a8763b7fbff30f703df1ea2b86710ff5b6641d
brucearthur1 <brucearthur1@bigpond.com>
2025-01-06 07:45:28 +0000

    added favicon.ico

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True) 
-  
+
