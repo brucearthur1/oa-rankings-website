3771176ed75abc4b02d2112d73c4afd54df9ac12
brucearthur1 <brucearthur1@bigpond.com>
2025-01-06 12:14:37 +0000

    Added dynamic data and API route

--- app.py
+++ app.py
@@ -3,9 +3,38 @@ from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
+ATHLETES = [
+  {
+      'id' : 1,
+      'name' : 'Michael Phelps',
+      'state' : 'SA',
+      'class' : 'Men',
+      'YOB' : 1985,
+      'points' : 5023
+  },  
+  {
+      'id' : 2,
+      'name' : 'John Smith',
+      'class' : 'Men',
+      'YOB' : 2005,
+      'points' : 5019
+  },  
+  {
+      'id' : 3,
+      'name' : 'Jane Smith',
+      'state' : 'WA',
+      'class' : 'Women',
+      'YOB' : 1995,
+      'points' : 4019
+  }
+    
+]
+
+
+
 @app.route("/")
 def home_page():
-    return render_template('index.html')
+    return render_template('index.html', athletes=ATHLETES, group='Women')
 
 @app.route('/favicon.png')
 def favicon():
